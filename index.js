d8fd31bac2ca4a51b04f591d72b4393a13fbcd78
HighSecurity <99303570+lowsecurity@users.noreply.github.com>
2023-11-02 16:01:05 +0000

    Update index.js
    35 FPS fix

--- index.js
+++ index.js
@@ -550,6 +550,6 @@ const sprites = {
 };
 
 const project = new Project(stage, sprites, {
-  frameRate: 60 // Set to 60 to make your project run faster
+  frameRate: 35 // Set to 60 to make your project run faster
 });
 export default project;
